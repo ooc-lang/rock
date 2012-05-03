0c7e66936544d8c216799077312748a52e31d945
Amos Wenger <amos@official.fm>
2012-05-03 05:07:27 +0200

    Array -> C89

--- sdk/lang/Array.h
+++ sdk/lang/Array.h
@@ -1,18 +1,9 @@
 #ifndef ___lang_array___
 #define ___lang_array___
 
-#ifdef __OOC_USE_GC__
-#define array_malloc GC_malloc
-#define array_free GC_free
-#else
-#define array_malloc malloc
-#define array_free free
-#endif // GC
-
 #include <stdint.h>
 
-
-#define _lang_array__Array_new(type, size) ((_lang_array__Array) { size, array_malloc(size * sizeof(type)) });
+#define _lang_array__Array_new(type, size) ((_lang_array__Array) { size, GC_malloc(size * sizeof(type)) });
 
 #define _lang_array__Array_get(array, index, type) ( \
     (index < 0 || index >= array.length) ? \
@@ -26,12 +17,12 @@
     *((type*) NULL) : \
     (((type* restrict) array.data)[index] = value)
 
-#define _lang_array__Array_free(array) { array_free(array.data) }
+#define _lang_array__Array_free(array) { GC_malloc_free(array.data) }
 
 typedef struct {
     size_t length;
     void* restrict data;
 } _lang_array__Array;
 
-#endif // ___lang_array___
+#endif
 
