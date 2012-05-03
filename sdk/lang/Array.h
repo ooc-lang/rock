159b0541976130c973f5f30e0657f06625cb7e23
Amos Wenger <amos@official.fm>
2012-05-03 06:09:14 +0200

    restrict is out, C89

--- sdk/lang/Array.h
+++ sdk/lang/Array.h
@@ -9,13 +9,13 @@
     (index < 0 || index >= array.length) ? \
     lang_Exception__Exception_throw((lang_Exception__Exception *) lang_Exception__OutOfBoundsException_new_noOrigin(index, array.length)), \
     *((type*) NULL) : \
-    ((type* restrict) array.data)[index])
+    ((type*) array.data)[index])
 
 #define _lang_array__Array_set(array, index, type, value) \
     (index < 0 || index >= array.length) ? \
     lang_Exception__Exception_throw((lang_Exception__Exception *) lang_Exception__OutOfBoundsException_new_noOrigin(index, array.length)), \
     *((type*) NULL) : \
-    (((type* restrict) array.data)[index] = value)
+    (((type*) array.data)[index] = value)
 
 #define _lang_array__Array_free(array) { GC_malloc_free(array.data) }
 
